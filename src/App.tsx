3e54ecefb6f67c42f24db0fdf19b037085804ef4
Samuel Silva <samuelsilvawb@gmail.com>
2023-04-09 12:10:22 +0100

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@stelli/components-library-experiment.atoms.button'
+
+function App() {
+  return (
+    
+      <header>
+        <Button>
+          Click me
+        </Button>
+      </header>
+    
+  );
+}
+
+export default App;

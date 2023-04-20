b654df9143b9fde52c505c5d30cf45466424702b
Sergio Vanacloig <sergiovanacloig97@gmail.com>
2023-04-20 10:54:56 +0200

    feat: Use both external components with and without inner dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
-import { PrimaryButton as Button } from '@stelli/components-library-experiment.atoms.primary-button'
+import { PrimaryButton } from "@stelli/components-library-experiment.atoms.primary-button";
+import { Button } from "@stelli/components-library-experiment.atoms.button";
+import { Minus as MinusIcon } from "@stelli/components-library-experiment.icons.minus";
 
 function App() {
   return (
-    
-      <header>
-        <Button iconName='minus'>
-          Click me
-        </Button>
-      </header>
-    
+    <header>
+      <PrimaryButton iconName="minus">
+        This is primary with inner dependencies
+      </PrimaryButton>
+      <br />
+      <Button icon={<MinusIcon />}>
+        This is button with no direct dependencies
+      </Button>
+    </header>
   );
 }
 
